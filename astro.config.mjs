44c57916ab1996c1018d2acf8aa94773815d6950
Elvis M. <u201112959@upc.edu.pe>
2025-05-29 09:56:53 -0500

    feat(auth): implement user authentication actions and middleware
    - Added login, logout, and register actions in the auth directory.
    - Created a centralized auth action index for easier imports.
    - Developed a middleware to manage user authentication state and access control.
    - Implemented Navbar component to handle user login state and navigation.
    - Created AuthLayout and MainLayout for consistent page structure.
    - Designed login and registration pages with form handling and validation.
    - Integrated SweetAlert for user feedback on authentication actions.
    - Updated TypeScript configuration for module path aliases.
    - Added global CSS styles using Tailwind CSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import auth from "auth-astro";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+  integrations: [auth()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
