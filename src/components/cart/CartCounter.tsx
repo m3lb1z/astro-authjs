91f6f10a7c364067c26e68ccb08202bd82d48dcc
Elvis M. <u201112959@upc.edu.pe>
2025-05-31 18:07:57 -0500

    feat: add CartCounter component and integrate it into the Navbar; implement product size selection and quantity adjustment in product detail page

---
+++ src/components/cart/CartCounter.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const CartCounter = () => {
+  return (
+    <a href="/cart" className="relative inline-block">
+      <span className="absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-5 h-5">
+        3
+      </span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M0 1h4.764l.545 2h18.078l-3.666 11H7.78l-.5 2H22v2H4.72l1.246-4.989L3.236 3H0zm4 20a2 2 0 1 1 4 0a2 2 0 0 1-4 0m14 0a2 2 0 1 1 4 0a2 2 0 0 1-4 0"
+        ></path>
+      </svg>
+    </a>
+  );
+};
